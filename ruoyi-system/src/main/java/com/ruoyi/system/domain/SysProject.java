33a297688b002188205febf559ad6fc1b7adf410
yang.jiao <yang.jiao@roadshare.com>
2020-02-24 18:01:01 +0800

    aa

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysProject.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysProject.java
@@ -1,8 +1,7 @@
 package com.ruoyi.system.domain;
 
 import com.ruoyi.common.core.domain.BaseEntity;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -13,12 +12,15 @@ import java.util.Date;
  * @author ruoyi
  * @date 2019-07-01
  */
+@Data
 public class SysProject extends BaseEntity
 {
 	private static final long serialVersionUID = 1L;
 	
 	/** 项目ID */
 	private Long projectId;
+	/** 项目编号 */
+	private String projectNumber;
 	/** 项目名称 */
 	private String projectName;
 	/** 客户ID */
@@ -36,101 +38,4 @@ public class SysProject extends BaseEntity
 	/** 是否删除 */
 	private Boolean delFlag;
 
-	public Byte getDealFlag() {
-		return dealFlag;
-	}
-
-	public void setDealFlag(Byte dealFlag) {
-		this.dealFlag = dealFlag;
-	}
-
-
-	public String getDeliveryDate() {
-		return deliveryDate;
-	}
-
-	public void setDeliveryDate(String deliveryDate) {
-		this.deliveryDate = deliveryDate;
-	}
-
-	public Boolean getDelFlag() {
-		return delFlag;
-	}
-
-	public void setDelFlag(Boolean delFlag) {
-		this.delFlag = delFlag;
-	}
-	public void setProjectId(Long projectId)
-	{
-		this.projectId = projectId;
-	}
-
-	public Long getProjectId() 
-	{
-		return projectId;
-	}
-	public void setProjectName(String projectName) 
-	{
-		this.projectName = projectName;
-	}
-
-	public String getProjectName() 
-	{
-		return projectName;
-	}
-	public void setCustomerId(Long customerId) 
-	{
-		this.customerId = customerId;
-	}
-
-	public Long getCustomerId() 
-	{
-		return customerId;
-	}
-	public void setStatus(Integer status) 
-	{
-		this.status = status;
-	}
-
-	public Integer getStatus() 
-	{
-		return status;
-	}
-	public void setAmount(BigDecimal amount) 
-	{
-		this.amount = amount;
-	}
-
-	public BigDecimal getAmount() 
-	{
-		return amount;
-	}
-	public void setPmUserId(Long pmUserId) 
-	{
-		this.pmUserId = pmUserId;
-	}
-
-	public Long getPmUserId()
-	{
-		return pmUserId;
-	}
-
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("projectId", getProjectId())
-            .append("projectName", getProjectName())
-            .append("customerId", getCustomerId())
-            .append("status", getStatus())
-            .append("amount", getAmount())
-            .append("pmUserId", getPmUserId())
-			.append("deliveryDate",getDeliveryDate())
-			.append("dealFlag",getDealFlag())
-			.append("delFlag",getDelFlag())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .append("remark", getRemark())
-            .toString();
-    }
 }
