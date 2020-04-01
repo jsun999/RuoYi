cb2be75a41c8d3ad04bc193226c1005b1ef5d63f
jsun999 <jsun999@126.com>
2020-04-02 00:25:48 +0800

    activiti

--- ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
@@ -1276,7 +1276,41 @@
             	    return null;
             	}
                 return array.join(separator);
-            }
+            },
+			// 获取form下所有的字段并转换为json对象
+			formToJSON: function(formId) {
+				var json = {};
+				$.each($("#" + formId).serializeArray(), function(i, field) {
+					json[field.name] = field.value;
+				});
+				return json;
+			}
+			// Xianlu Tech 扩展 v1.0
+			/**
+			 * 计算时间差，单位秒
+			 * @param startDateStr
+			 * @param endDateStr
+			 */
+			,calcTotalSecond: function(startDateStr, endDateStr) {
+				var date1 = new Date(startDateStr);             // 开始时间
+				var date2 = new Date(endDateStr);               // 结束时间
+				var timeSub = date2.getTime() - date1.getTime();  // 时间差毫秒
+				return timeSub / 1000;
+			},
+			/**
+			 * 计算出相差天数
+			 * @param secondSub
+			 */
+			formatTotalDateSub: function(secondSub) {
+				var days = Math.floor(secondSub / (24 * 3600));     // 计算出小时数
+				var leave1 = secondSub % (24*3600) ;                // 计算天数后剩余的毫秒数
+				var hours = Math.floor(leave1 / 3600);              // 计算相差分钟数
+				var leave2 = leave1 % (3600);                       // 计算小时数后剩余的毫秒数
+				var minutes = Math.floor(leave2 / 60);              // 计算相差秒数
+				var leave3 = leave2 % 60;                           // 计算分钟数后剩余的毫秒数
+				var seconds = Math.round(leave3);
+				return days + " 天 " + hours + " 时 " + minutes + " 分 " + seconds + ' 秒';
+			}
         }
     });
 })(jQuery);
--- ruoyi-process/src/main/java/com/ruoyi/process/leave/domain/BizLeaveVo.java
+++ ruoyi-process/src/main/java/com/ruoyi/process/leave/domain/BizLeaveVo.java
@@ -1,7 +1,10 @@
 package com.ruoyi.process.leave.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class BizLeaveVo extends BizLeave {
 
     /** 申请人姓名 */
@@ -19,43 +22,4 @@ public class BizLeaveVo extends BizLeave {
     /** 创建人 */
     private String createUserName;
 
-    public String getApplyUserName() {
-        return applyUserName;
-    }
-
-    public void setApplyUserName(String applyUserName) {
-        this.applyUserName = applyUserName;
-    }
-
-    public String getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(String taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public Date getDoneTime() {
-        return doneTime;
-    }
-
-    public void setDoneTime(Date doneTime) {
-        this.doneTime = doneTime;
-    }
-
-    public String getCreateUserName() {
-        return createUserName;
-    }
-
-    public void setCreateUserName(String createUserName) {
-        this.createUserName = createUserName;
-    }
 }
